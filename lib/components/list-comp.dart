a616eccd20649d69ad20469e4f89cef82f6241f7
zero9527 <zero8297183@163.com>
2019-09-19 20:01:48 +0800

    ListView封装：下拉刷新、上拉加载

--- lib/components/list-comp.dart
+++ lib/components/list-comp.dart
@@ -1,68 +1,101 @@
 import 'package:flutter/material.dart';
 
-class ListComp extends StatelessWidget {
+class ListComp extends StatefulWidget {
   final int page; // 页码
+  final EdgeInsetsGeometry margin;
+  final EdgeInsetsGeometry padding;
+  final BoxDecoration decoration;
   final List dataList; // 数据
   final bool hasMore; //  还有更多数据
+  final bool skeletonLoading; // 开始时使用骨架屏 loading
   final String loadingText; // 请求时的提示
+  final Widget loadingWidget; // 请求时的显示 Widget 
   final String nomoreText; // 没有更多数据的提示
   final Function listItemBuilder; // 每项显示的解构
   final Function itemClick; // 每项点击
   final Function onRefresh; // 下拉刷新
   final Function onLoadMore; // 上滑加载
 
-  final ScrollController _scrollController = ScrollController();
-
   ListComp({
     Key key,
     @required this.dataList,
     @required this.hasMore,
     this.page = 1,
+    this.decoration,
+    this.margin = const EdgeInsets.all(10),
+    this.padding = const EdgeInsets.all(10),
+    this.skeletonLoading = false,
     this.loadingText = '拼命加载中...',
-    this.nomoreText = '--- 我是有底线的 ---',
+    this.loadingWidget,
+    this.nomoreText = '----- 我是有底线的 -----',
     this.listItemBuilder,
     this.itemClick,
     @required this.onRefresh,
     @required this.onLoadMore,
-  }) {
+  }) : super(key: key);
+
+  @override
+  ListState createState() => ListState();
+}
+
+class ListState extends State<ListComp> {
+  List _dataList = [];
+  bool isReachBottom = true; // 是否到达底部
+  final ScrollController _scrollController = ScrollController();
+
+  ListState() : super() {
     _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-        _scrollController.position.maxScrollExtent) {
+      if (listReachBottom()) {
         onLoadMore();
+
+      } else {
+        setState(() {
+          isReachBottom = false;
+        });
       }
     });
   }
 
+  // 是否到达底部
+  bool listReachBottom() {
+    return _scrollController.position.pixels == _scrollController.position.maxScrollExtent;
+  }
+
   void listItemClick(item) {
-    if (itemClick != null) itemClick(item);
+    if (widget.itemClick != null) widget.itemClick(item);
+  }
+
+  void onLoadMore() {
+    setState(() {
+      isReachBottom = true;
+    });
+    widget.onLoadMore();
+  }
+
+  Future<void> onRefresh() {
+    setState(() {
+      isReachBottom = true;
+    });
+    return widget.onRefresh();
   }
 
   Widget listItem(item, int index) {
     return Container(
-      padding: index == 0 
-        ? const EdgeInsets.fromLTRB(8, 10, 8, 10)
-        : const EdgeInsets.fromLTRB(8, 0, 8, 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(4)),
-        boxShadow: [BoxShadow(
-          color: Color.fromRGBO(0, 0, 0, .4),
-          offset: Offset(0, 0.5),
-          blurRadius: 1.0,
-          spreadRadius: -10.0
-        )]
-      ),
+      margin: widget.margin,
+      decoration: widget.decoration != null 
+        ? widget.decoration
+        : BoxDecoration(
+          color: Colors.white,
+        ),
       child: FlatButton(
-        padding: const EdgeInsets.all(20),
+        padding: widget.padding,
         onPressed: () => listItemClick(item),
-        color: Colors.white,
-        child: Container(
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: listItemBuilder(item),
-              )
-            ],
-          ),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: widget.listItemBuilder(item),
+            ),
+          ],
         ),
       ),
     );
@@ -73,33 +106,78 @@ class ListComp extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text(hasMore ? '$loadingText' : '$nomoreText' , style: TextStyle(
-            color: Colors.grey
-          ),)
+          Visibility(
+            visible: widget.hasMore && isReachBottom,
+            child: SizedBox(
+              width: 20,
+              height: 20,
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+                valueColor: AlwaysStoppedAnimation(Colors.grey),
+              ),
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 10),
+            child: widget.hasMore 
+              ? (widget.loadingWidget != null 
+                ? isReachBottom ? widget.loadingWidget : greyText('上滑加载更多')
+                : isReachBottom ? greyText(widget.loadingText) : greyText('上滑加载更多'))
+              : greyText(widget.nomoreText)
+          )
         ],
       ),
     );
   }
 
+  Widget greyText(String text) {
+    return Text('$text', 
+      style: TextStyle(color: Colors.grey)
+    );
+  }
+
+  @override
+  void didUpdateWidget(ListComp oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.dataList.length != widget.dataList.length) return;
+    Future.delayed(Duration(seconds: 2), () {
+      // 一页数量太少，没有滚动条，导致ListView不能触发上滑
+      if (
+        widget.page == 1
+        && _scrollController.position.maxScrollExtent == 0
+      ) {
+        onLoadMore();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      if (_dataList.length > 0 && _dataList[_dataList.length-1][0] == null) _dataList.removeLast();
+      if (_dataList.length > 1) _dataList.clear();
+      _dataList.addAll(widget.dataList);
+      _dataList.add({});
+    });
+
     return RefreshIndicator(
       onRefresh: onRefresh,
-      child: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              controller: _scrollController,
-              itemCount: dataList.length,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == dataList.length-1) return loadingItem();
-                return listItem(dataList[index], index);
-              }
-            ),
-          ),
-        ],
-      )
+      child: ListView.builder(
+        controller: _scrollController,
+        physics: BouncingScrollPhysics(),
+        itemCount: _dataList.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (
+            widget.dataList.length == 0 ||
+            index == _dataList.length-1
+          ) {
+            return loadingItem();
+          }
+          return listItem(_dataList[index], index);
+        }
+      ),
     );
   }
 }
--- lib/views/ListPage/index.dart
+++ lib/views/ListPage/index.dart
@@ -14,12 +14,12 @@ class ListPage extends StatefulWidget {
 }
 
 class ListState extends State<ListPage> {
-  int page = 1;
+  int page = 0;
   List dataList = [];
   bool hasMore = true;
   final initialList = [
     { 'id': 1, 'text': 'list item 1list item 1list item 1list item 1list item 1list item 1list item 1list item 1' },
-    { 'id': 2, 'text': 'list item 2' },
+    { 'id': 2, 'text': 'list item 2list item 2list item 2list item 2list item 2list item 2list item 2' },
     { 'id': 3, 'text': 'list item 3' },
     { 'id': 4, 'text': 'list item 4' },
     { 'id': 5, 'text': 'list item 5' },
@@ -28,14 +28,13 @@ class ListState extends State<ListPage> {
     { 'id': 8, 'text': 'list item 8' },
     { 'id': 9, 'text': 'list item 9' },
     { 'id': 10, 'text': 'list item 10' },
-    {}
   ];
 
   @override
   void initState() {
     super.initState();
-    setState(() {
-      dataList.addAll(initialList);
+    Future.delayed(Duration(seconds: 1), () {
+      loadMore();
     });
   }
 
@@ -46,6 +45,7 @@ class ListState extends State<ListPage> {
     });
     await Future.delayed(const Duration(seconds: 1), () {
       setState(() {
+        // dataList = initialList;
         dataList.clear();
         dataList.addAll(initialList);
       });
@@ -57,33 +57,27 @@ class ListState extends State<ListPage> {
     setState(() {
       ++page;
     });
-    final moreList = [
-      { 'id': 11, 'text': 'list item 11' },
-      { 'id': 12, 'text': 'list item 12' },
-      { 'id': 13, 'text': 'list item 13' },
-      { 'id': 14, 'text': 'list item 14' },
-      { 'id': 15, 'text': 'list item 15' },
-    ];
-
-    List(5).map((item) {
-      print(item);
+    final moreList = [];
+    List.generate(10, (int index) {
+      moreList.add(
+        { 'id': dataList.length+index+1, 'text': 'list item ${dataList.length+index+1}' }
+      );
     });
 
-    await Future.delayed(const Duration(seconds: 1), () {
+    Future.delayed(const Duration(seconds: 1), () {
       setState(() {
-        dataList.removeLast();
         dataList.addAll(moreList);
-        dataList.add({});
-        if (page == 3) hasMore = false;
+        if (page >= 3) hasMore = false;
       });
     });
+
   }
 
   void toDetail(item) {
     Navigator.pushNamed(context, '/detail', arguments: { 'id': item['id'], 'text': item['text'] });
   }
 
-  Widget listItemChild(item) {
+  Widget listItemBuilder(item) {
     return Text('${item['text']}');
   }
 
@@ -98,12 +92,20 @@ class ListState extends State<ListPage> {
           children: <Widget>[
             ListComp(
               page: page,
+              margin: const EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue
+                ),
+              ),
+              // padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
               hasMore: hasMore,
               dataList: dataList,
-              itemClick: (item) => toDetail(item),
+              skeletonLoading: true,
+              // itemClick: (item) => toDetail(item),
               onRefresh: onRefresh,
               onLoadMore: loadMore,
-              listItemBuilder: (item) => listItemChild(item),
+              listItemBuilder: (item) => listItemBuilder(item),
             ),
             Positioned(
               bottom: 30,
@@ -126,7 +128,7 @@ class ListState extends State<ListPage> {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
                     Text('$page/', style: TextStyle(fontSize: 20, color: Colors.black45),),
-                    Text('${dataList.length-1}', style: TextStyle(color: Colors.black26),)
+                    Text('${dataList.length}', style: TextStyle(color: Colors.black26),)
                   ],
                 ),
               )
